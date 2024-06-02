617ee6156bf4345998f777d2e381557b6f03e85c
ThanhTrung22 <anhtrungpk11@gmail.com>
2024-06-02 15:23:55 +0700

    definition get_input_num

--- src/function.c
+++ src/function.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
-void get_input_num(int *num)
+void get_input_num(int32_t *num)
 {
-    
+    int8_t check = 0;
+    do
+    {
+        printf("\nNhap 1 so khong am: ");
+        check = ("%d", num);
+        while(getchar() != '\n');
+    }while(num < 0 || check == 0);
 }
